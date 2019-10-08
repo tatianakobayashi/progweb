49c834dba89ab913a9561bdadac6744a22353fc0
Tatiana Oshiro Kobayashi <42660441+tatianakobayashi@users.noreply.github.com>
2019-10-08 11:22:37 -0400

    Update Usuario.php
    adicionando getters e setters

--- Models/Usuario.php
+++ Models/Usuario.php
@@ -1,9 +1,13 @@
 <?php
 	/**
 	 *  Usuário
+	 *
+	 *	- Getters e setters prontos
+	 *
+	 * 	TODO: 
+	 *	- Criptografia das senhas (?)
 	 */
 	class Usuario{
-
 		private $nome;
 		private $email;
 		private $cpf;
@@ -16,6 +20,56 @@
 			$this->email = $email;
 			$this->cpf = $cpf;
 			$this->senha = $senha;
+			$this->telefone = $telefone;
+		}
+
+		public function getNome()
+		{
+			return $this->nome;
+		}
+
+		public function setNome($nome)
+		{
+			$this->nome = $nome;
+		}
+
+		public function getEmail()
+		{
+			return $this->email;
+		}
+
+		public function setEmail($email)
+		{
+			$this->email = $email;
+		}
+
+		public function getCpf()
+		{
+			return $this->cpf;
+		}
+
+		public function setCpf($cpf)
+		{
+			$this->cpf = $cpf;
+		}
+
+		public function getSenha()
+		{
+			return $this->senha;
+		}
+
+		public function setSenha($senha)
+		{
+			$this->senha = $senha;
+		}
+
+		public function getTelefone()
+		{
+			return $this->telefone;
+		}
+
+		public function setTelefone($telefone)
+		{
 			$this->telefone = $telefone;
 		}
 	}
