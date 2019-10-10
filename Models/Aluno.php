315ca2cae3080ac392ea5af8681f4844e02fd38b
Tatiana Oshiro Kobayashi <42660441+tatianakobayashi@users.noreply.github.com>
2019-10-10 09:24:53 -0400

    Update Aluno.php
    setters/getters

--- Models/Aluno.php
+++ Models/Aluno.php
@@ -16,5 +16,32 @@
 			$this->ano = $ano;
 			$this->dataNasc = $dataNasc;
 		}
+
+		public function getRA()
+		{
+			return $this->RA;
+		}
+		public function setRA($RA)
+		{
+			$this->RA = $RA;
+		}
+
+		public function getAno()
+		{
+			return $this->ano;
+		}
+		public function setAno($ano)
+		{
+			$this->ano = $ano;
+		}
+
+		public function getDataNasc()
+		{
+			return $this->dataNasc;
+		}
+		public function setDataNasc($dataNasc)
+		{
+			$this->dataNasc = $dataNasc;
+		}
 	}
 ?>
